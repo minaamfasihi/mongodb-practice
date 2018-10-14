2218d2ca4816267ac982e4ffd9d0aacda0565e8e
minaamfasihi <minaam.fasihi@gmail.com>
2018-10-14 22:38:53 +0500

    Added tests

--- test/test_helper.js
+++ test/test_helper.js
@@ -6,3 +6,13 @@ mongoose.connection
 	.on('error', (error) => {
 		console.warn('Warning', error);
 	});
+
+
+// describe, it and beforeEach are all called with a done callback which, when called, signals to mocha that it can continue 
+// running further tests
+
+beforeEach((done) => {
+	mongoose.connection.collections.users.drop(() => {
+		done();
+	});
+});
